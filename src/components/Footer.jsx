99d005b1ed7dd920f581bf6494871ad7f54c228d
madesh02104 <madesh02104@gmail.com>
2025-01-30 20:22:08 +0530

    finished portfolia site

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,17 +5,19 @@ import { SiLeetcode, SiGeeksforgeeks } from "react-icons/si";
 
 const Footer = () => {
   return (
-    <section className='bg-emerald-600 w-full pt-6 rounded-t-full'>
-      <p className='mx-[30%] mb-6 text-white font-semibold text-2xl mx-auto'>Learning today and making one step at a time.</p>
-      <div className='social-links mx-[39%] flex gap-8'>
-        <a href=""> <FaGithub size={32} /></a>
-        <a href=""> <FaTwitter size={32}/></a>
-        <a href=""> <SiLeetcode size={32}/></a>
-        <a href=""> <SiGeeksforgeeks size={32}/></a>
-        <a href=""> <FaReddit size={32}/></a>
+    <section>
+      <section className='bg-emerald-600 w-full pt-6 rounded-t-2xl flex flex-col justify-center items-center gap-6'>
+      <p className='text-white  font-semibold text-md sm:text-2xl'>Learning today and making one step at a time.</p>
+      <div className='w-fit flex gap-8 mb-4'>
+        <a href="https://github.com/madesh02104" target='_blank'> <FaGithub size={32} /></a>
+        <a href="https://x.com/madesh02104" target='_blank'> <FaTwitter size={32}/></a>
+        <a href="https://leetcode.com/u/IMqC2Ksi4Y/" target='_blank'> <SiLeetcode size={32}/></a>
+        <a href="https://www.geeksforgeeks.org/user/madeshlvc3/" target='_blank'> <SiGeeksforgeeks size={32}/></a>
       </div>
-
     </section>
+            <p className='text-md italic text-emerald-1000 text-center'>&copy; Built by <a href='https://github.com/madesh02104' target='_blank'>@Madesh</a></p>
+    </section>
+
   )
 }
 
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import heroImage from '../assets/hero-image.png'
+import avatar from '../assets/avatar.png'
 
 const Hero = () => {
   return (
     <section className='w-[80%] mt-40 flex flex-col justify-center items-center gap-4'>
+        <img src={avatar} alt="avatar" className='h-48' />
         <h2 className='text-3xl sm:text-4xl md:text-5xl text-center font-bold text-gray-800'>Frontend Developer, Full-Stack Aspirer.</h2>
         <h6 className='text-lg sm:text-xl text-center'>I design and code simple yet beautiful web applicatons.</h6>
         <img src={heroImage} alt="hero-image" title='Coding...' className='h-48 w-auto mt-64 sm:h-64 lg:h-96 ' />
